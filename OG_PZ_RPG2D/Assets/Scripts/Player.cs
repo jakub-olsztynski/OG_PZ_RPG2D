2033be0aa382f40e789912896653680675c91d2a
Emil25G <emil.gielek@gmail.com>
2023-11-19 19:34:04 +0100

    Dodałem Player_Camera Cinemamachine
    To jest dodatek w Package Manager, który można pobrać. Bardzo łatwo się go używa i ma wiele opcji bez programowania

--- OG_PZ_RPG2D/Assets/Scripts/Player.cs
+++ OG_PZ_RPG2D/Assets/Scripts/Player.cs
@@ -28,20 +28,20 @@ public class Player : MonoBehaviour
         else if (moveDelta.x < 0)
             transform.localScale = new Vector3(-1, 1, 1);
 
-        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(0, moveDelta.y), Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(0, moveDelta.y), Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking")); //LayerMask pozwala na blokowanie wybranych warstw w zakresie y
         if (hit.collider == null)
         {
             //Moving
             transform.Translate(0, moveDelta.y * Time.deltaTime, 0);
         }
 
-        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(moveDelta.x, 0), Mathf.Abs(moveDelta.x * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(moveDelta.x, 0), Mathf.Abs(moveDelta.x * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking")); //LayerMask pozwala na blokowanie wybranych warstw w zakresie x
         if (hit.collider == null)
         {
             //Moving
             transform.Translate(moveDelta.x * Time.deltaTime, 0, 0);
         }
-
+        //Warto zwróciæ uwagê ¿e obecnie wg kodu gracz ma kolizjê z samym sob¹ daltego aby tego unik¹æ prze³aczyæ trzeba opcjê: Edit>ProjectSettings>Physics2D>Queries Start in Collider
 
     }
 }
