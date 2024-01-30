af927802dbd87648ce9bf701c5844418782012dd
jakub-olsztynski <126956736+jakub-olsztynski@users.noreply.github.com>
2024-01-30 16:49:57 +0100

    Update Fighter.cs

--- OG_PZ_RPG2D/Assets/Scripts/Fighter.cs
+++ OG_PZ_RPG2D/Assets/Scripts/Fighter.cs
@@ -7,5 +7,8 @@ public class Fighter : MonoBehaviour
     public int hitpoint = 10;
     public int maxHitpoint = 10;
     public float pushRecoverySpeed = 0.2f;
+    //niesmiertelnosc latency
+    public float immuneTime = 1.0f;
+    public float lastImmune;
 
 }
